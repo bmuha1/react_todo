38338b10da8f7a766918b6705457e46fa392a14f
Brent Muha <800@holbertonschool.com>
2020-05-12 11:10:59 -0700

    Add submit functionality

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Header from './components/layout/Header';
 import Todos from './components/Todos';
 import AddTodo from './components/AddTodo';
+import {v4 as uuid} from 'uuid';
 
 import './App.css';
 
@@ -9,16 +10,16 @@ class App extends Component {
   state = {
     todos: [
       {
-        id: 1,
+        id: uuid(),
         title: 'Take out the trash',
         completed: false
       },
       {
-        id: 2,
+        id: uuid(),
         title: 'Dinner with family',
         completed: true
       },       {
-        id: 3,
+        id: uuid(),
         title: 'Meeting with boss',
         completed: false
       }
@@ -41,12 +42,22 @@ class App extends Component {
       !== id)] });
   }
 
+  // Add Todo
+  addTodo = (title) => {
+    const newTodo = {
+      id: uuid(),
+      title,
+      completed: false
+    }
+    this.setState({ todos: [...this.state.todos, newTodo] });
+  }
+
   render() {
     return (
       <div className="App">
         <div className="container">
         <Header />
-        <AddTodo />
+        <AddTodo addTodo={this.addTodo} />
         <Todos todos={this.state.todos} markComplete={this.markComplete}
         delTodo={this.delTodo} />
         </div>
